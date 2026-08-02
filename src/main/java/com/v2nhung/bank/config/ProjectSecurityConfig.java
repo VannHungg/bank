d31ea26ab93cd2f60d69ac3f4d748d697b0053af
v2n.hung <vanhunglekhiet@gmail.com>
2026-08-02 14:24:58 +0700

    delete file not need in flow with keycloak

--- src/main/java/com/v2nhung/bank/config/ProjectSecurityConfig.java
+++ src/main/java/com/v2nhung/bank/config/ProjectSecurityConfig.java
@@ -1,24 +1,14 @@
 package com.v2nhung.bank.config;
 
-import com.v2nhung.bank.filter.AuthoritiesLogginAfterFilter;
 import com.v2nhung.bank.filter.CsrfTokenFilter;
-import com.v2nhung.bank.filter.JWTTokenGeneratorFilter;
-import com.v2nhung.bank.filter.JWTTokenValidatorFilter;
-import com.v2nhung.bank.filter.RequestValidationBeforeAuthenFilter;
-import com.v2nhung.bank.handler.OAuth2AuthenticationSuccessHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -28,10 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import java.util.Collections;
 
 @Configuration
-@RequiredArgsConstructor
 public class ProjectSecurityConfig {
-
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -57,11 +44,7 @@ public class ProjectSecurityConfig {
                         .ignoringRequestMatchers("/contact", "/register")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
-                .addFilterBefore(new RequestValidationBeforeAuthenFilter(), BasicAuthenticationFilter.class)
                 .addFilterAfter(new CsrfTokenFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthoritiesLogginAfterFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
 
                 // authorization
@@ -72,9 +55,7 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/myCards").hasRole("USER")
                         .requestMatchers("/user").authenticated()
                         .anyRequest().permitAll()
-                )
-                .oauth2Login(oauth2 ->
-                        oauth2.successHandler(oAuth2AuthenticationSuccessHandler));
+                );
 
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
@@ -93,19 +74,5 @@ public class ProjectSecurityConfig {
                 "/.well-known/**",
                 "/error"
         );
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration github = githubClientRegistration();
-        return new InMemoryClientRegistrationRepository(github);
-    }
-
-    private ClientRegistration githubClientRegistration() {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github")
-                .clientId("Ov23ligggCnYyP3ifSx3")
-                .clientSecret("c47b3d8efc1baca7665e888adc3ba6447bd241ca")
-                .scope("read:user", "user:email")
-                .build();
     }
 }
--- src/main/java/com/v2nhung/bank/constant/BankConstant.java
+++ src/main/java/com/v2nhung/bank/constant/BankConstant.java
@@ -1,7 +1,5 @@
 package com.v2nhung.bank.constant;
 
 public class BankConstant {
-    public static final String JWT_HEADER = "Authorization";
-    public static final String JWT_SECRET_VALUE = "jwt.secret.value";
-    public static final String JWT_SECRET_DEFAULT = "jwt.secret.default";
+
 }
